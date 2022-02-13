a7fede7e68277480e2a448d61ed1eb2147bdee2d
maidoudouo <maidoudouo@outlook.com>
2022-02-14 02:12:22 +0800

    Add listen port setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 ipset -R <chnroute.ipset
 ipset -R <chnroute6.ipset
 nohup crond -l 2 -f >/dev/null 2>&1 &
-chinadns-ng -b 0.0.0.0 -l 53 -c ${CHINA_DNS} -t ${TRUCT_DNS} -g /etc/chinadns-ng/gfwlist.txt -m /etc/chinadns-ng/chnlist.txt -o 3 -r
+chinadns-ng -b 0.0.0.0 -l ${PORT} -c ${CHINA_DNS} -t ${TRUCT_DNS} -g /etc/chinadns-ng/gfwlist.txt -m /etc/chinadns-ng/chnlist.txt -o 3 -r
